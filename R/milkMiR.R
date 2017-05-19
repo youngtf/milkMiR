3999b60ca9eaafab8f676535a24937a45b03be15
Tianfu Young <youngtim@Tianfu-MBP.local>
2017-05-19 16:07:35 -0600

    Initialization

---
+++ R/milkMiR.R
@@ -0,0 +1,16 @@
+#' Management of milk mid-infrared (MIR) spectra data
+#'
+#' milkMiR is a package to manage milk mid-infrared (MIR) spectra data
+#'
+#' @author
+#' Tianfu Yang \email{tianfu@ualberta.ca}
+#' Zhiquan Wang \email{zhiquan@ualberta.ca}
+#'
+#' Maintainer: Tianfu Yang \email{tianfu@ualberta.ca}
+#'
+#' @name milkMiR
+#' @docType package
+# @importFrom graphics axis plot points
+# @import dplyr
+
+NULL
